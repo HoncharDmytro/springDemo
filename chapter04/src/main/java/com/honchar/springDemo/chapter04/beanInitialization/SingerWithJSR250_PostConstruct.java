42e3510ec416bc89655c0c714df34accdba31cf2
Dmytro Honchar <dmytro.honchar972@gmail.com>
2022-05-22 16:57:08 +0300

    Updated chapter four(beanInitialization).

--- chapter04/src/main/java/com/honchar/springDemo/chapter04/beanInitialization/SingerWithJSR250_PostConstruct.java
+++ chapter04/src/main/java/com/honchar/springDemo/chapter04/beanInitialization/SingerWithJSR250_PostConstruct.java
@@ -1,10 +1,12 @@
-package com.honchar.springDemo.chapter04;
+package com.honchar.springDemo.chapter04.beanInitialization;
 
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-public class Singer {
+import javax.annotation.PostConstruct;
+
+public class SingerWithJSR250_PostConstruct {
     private static final String DEFAULT_NAME = "Eric Clapton";
     private String name;
     private int age = Integer.MIN_VALUE;
@@ -12,42 +14,42 @@ public class Singer {
     public void setName(String name) {
         this.name = name;
     }
-
-    public void setAge ( int age) {
+    public void setAge(int age) {
         this.age = age;
     }
 
-    public void init() {
+    @PostConstruct
+    public void init() throws Exception {
         System.out.println("Initializing bean");
         if (name == null) {
             System.out.println("Using default name");
             name = DEFAULT_NAME;
         }
         if (age == Integer.MIN_VALUE) {
-            throw new IllegalArgumentException("You must set the age property of any beans of type " + Singer.class);
+            throw new IllegalArgumentException("You must set the age property of any beans of type "
+                    + SingerWithJSR250_PostConstruct.class);
         }
     }
 
-    public String toString () {
+    public String toString() {
         return "\tName: " + name + "\n\tAge: " + age;
     }
 
-    public static Singer getBean(String beanName, ApplicationContext ctx) {
+    public static SingerWithJSR250_PostConstruct getBean(String beanName, ApplicationContext ctx) {
         try {
-            Singer bean = (Singer) ctx.getBean(beanName);
+            SingerWithJSR250_PostConstruct bean = (SingerWithJSR250_PostConstruct) ctx.getBean(beanName);
             System.out.println(bean);
             return bean;
         } catch (BeanCreationException ex) {
-            System.out.println("An error occured in bean configuration: " + ex.getMessage());
+            System.out.println("An error occurred in bean configuration: " + ex.getMessage());
             return null;
         }
     }
 
-    public static void main (String[] args) {
+    public static void main(String... args){
         GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-        ctx.load("classpath:spring/app-context.xml");
+        ctx.load("classpath:spring/app-context-annotation.xml");
         ctx.refresh();
-
         getBean("singerOne", ctx);
         getBean("singerTwo", ctx);
         getBean("singerThree", ctx);
