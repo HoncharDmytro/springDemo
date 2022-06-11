6d8cb4693ce84090c6845323962e74dcfbe2c613
Dmytro Honchar <dmytro.honchar972@gmail.com>
2022-06-11 14:01:46 +0300

    Added new entities and configurations to base hibernate program in chapter seven.

---
+++ chapter07/src/main/java/com/honchar/springDemo/chapter07/hibernate_base/config/AppConfig.java
@@ -0,0 +1,83 @@
+package com.honchar.springDemo.chapter07.hibernate_base.config;
+
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "com.honchar.springDemo.chapter07")
+@EnableTransactionManagement
+@PropertySource("classpath:db/jdbc2.properties")
+public class AppConfig {
+
+    private static Logger logger = LoggerFactory.getLogger(AppConfig.class);
+    Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        try {
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName(env.getProperty("driverClassName"));
+            dataSource.setUrl(env.getProperty("url"));
+            dataSource.setUsername(env.getProperty("username"));
+            dataSource.setPassword(env.getProperty("password"));
+
+//            Resource script1 = new ClassPathResource("classpath:sql/schema.sql");
+//            Resource script2 = new ClassPathResource("classpath:sql/test-data.sql");
+//            DatabasePopulator databasePopulator = new ResourceDatabasePopulator(script1, script2);
+//            DatabasePopulatorUtils.execute(databasePopulator, dataSource);
+
+            return dataSource;
+
+        } catch (Exception e) {
+            logger.error("DataSource bean cannot be created!", e);
+            return null;
+        }
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProp = new Properties();
+        hibernateProp.put("hibernate.dialect", env.getProperty("dialect"));
+        hibernateProp.put("hibernate.format_sql", true);
+        hibernateProp.put("hibernate.use_sql_comments", true);
+        hibernateProp.put("hibernate.show_sql", true);
+        hibernateProp.put("hibernate.max_fetch_depth", 3);
+        hibernateProp.put("hibernate.jdbc.batch_size", 10);
+        hibernateProp.put("hibernate.jdbc.fetch_size", 50);
+        return hibernateProp;
+    }
+
+    @Bean public SessionFactory sessionFactory() throws IOException {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("com.honchar.springDemo.chapter07.hibernate_base.entities");
+        sessionFactoryBean.setHibernateProperties(hibernateProperties());
+        sessionFactoryBean.afterPropertiesSet();
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean public PlatformTransactionManager transactionManager() throws IOException {
+        return new HibernateTransactionManager(sessionFactory());
+    }
+}
---
+++ chapter07/src/main/java/com/honchar/springDemo/chapter07/hibernate_base/entities/Instrument.java
@@ -0,0 +1,40 @@
+package com.honchar.springDemo.chapter07.hibernate_base.entities;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "instrument")
+public class Instrument implements Serializable {
+    private String instrumentId;
+    private Set<Singer> singers = new HashSet<>();
+
+    @Id
+    @Column(name = "instrument_id")
+    public String getInstrumentId() {
+        return this.instrumentId;
+    }
+
+    @ManyToMany
+    @JoinTable(name = "singer_instrument",
+            joinColumns = @JoinColumn(name = "instrument_id"),
+            inverseJoinColumns = @JoinColumn(name = "singer_id"))
+    public Set<Singer> getSingers() {
+        return this.singers;
+    }
+
+    public void setSingers(Set<Singer> singers) {
+        this.singers = singers;
+    }
+
+    public void setInstrumentId(String instrumentId) {
+        this.instrumentId = instrumentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Instrument :" + getInstrumentId();
+    }
+}
